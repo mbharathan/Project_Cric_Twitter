2071f05d89fe58e7982fd98b1ef5507a5e9780c3
Bharathan.M <mbharathan@outlook.com>
2020-10-17 13:11:08 +0530

    Added Import os

--- tech.py
+++ tech.py
@@ -1,4 +1,5 @@
 import twitter
+import os
 api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
                                           consumer_secret=os.environ.get('CONSUMER_SECRET'),
                                           access_token_key=os.environ.get('ACCESS_TOKEN_KEY'),
