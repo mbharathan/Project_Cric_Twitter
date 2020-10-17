e8cf4659dde0a2a82bd5b2001f76ecd2666efa4c
Bharathan.M <mbharathan@outlook.com>
2020-10-17 13:07:09 +0530

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn senthil_andavar.wsgi
---
+++ tech.py
@@ -0,0 +1,8 @@
+import twitter
+api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
+                                          consumer_secret=os.environ.get('CONSUMER_SECRET'),
+                                          access_token_key=os.environ.get('ACCESS_TOKEN_KEY'),
+                                          access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET'))
+
+status = api.PostUpdate("Hey!")
+print("Tweeted as " + status.text)
